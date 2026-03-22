3718777832dd49a7ff192bc289c8587a96d1372b
burhan-123456 <fanaak596@gmail.com>
2026-03-22 12:14:15 +0530

    fixed bugs

--- app.py
+++ app.py
@@ -4,19 +4,16 @@ import string
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 import nltk
+import os
 
+# ---------------- NLTK FIX (IMPORTANT) ----------------
+nltk_data_path = "/tmp/nltk_data"
+os.makedirs(nltk_data_path, exist_ok=True)
+nltk.data.path.append(nltk_data_path)
 
-# ✅ Add this here
-try:
-    nltk.data.find('tokenizers/punkt')
-except LookupError:
-    nltk.download('punkt')
-    nltk.download('punkt_tab')
-
-try:
-    nltk.data.find('corpora/stopwords')
-except LookupError:
-    nltk.download('stopwords')
+nltk.download('punkt', download_dir=nltk_data_path)
+nltk.download('punkt_tab', download_dir=nltk_data_path)
+nltk.download('stopwords', download_dir=nltk_data_path)
 
 # Load models
 tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
